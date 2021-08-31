67ef815f3e1845752e1b89566684a8a8f1f6fb12
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-08-31 19:03:34 +0300

    done with django models lab 27

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,4 +1,17 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
 # ok
+
+class SnacksTests(TestCase):
+    def test_snake_list_page_status_code(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_snake_list_page_templete(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
--- snacks/views.py
+++ snacks/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.urls import path
-from .views import SnackDetailView, SnackListView
+from django.db import models
+from .models import Snack
+from django.views.generic import ListView, DetailView
+# from django.views.generic.detail import DetailView
 
 # Create your views here.
 
-urlpatterns=[
-    path('',SnackListView.as_view(), name='snack_list'),
-    path('snack_detail/<int:pk>/', SnackDetailView.as_view(), name='snack_detail')
-]
+class SnackListView(ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack    
