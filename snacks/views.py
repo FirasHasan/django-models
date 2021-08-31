88e3c36ad2ce277130e213ddb004f0987644253b
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-08-31 07:28:02 +0300

    initial committt not

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.urls import path
+from .views import SnackDetailView, SnackListView
 
 # Create your views here.
+
+urlpatterns=[
+    path('',SnackListView.as_view(), name='snack_list'),
+    path('snack_detail/<int:pk>/', SnackDetailView.as_view(), name='snack_detail')
+]
